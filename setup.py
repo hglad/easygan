25450dcbf655ff4c0bbf8b6b2945404a6fafba2d
hglad <hanseglad@gmail.com>
2021-09-12 13:23:56 +0200

    Add required packages to setup

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 import setuptools
 
 setuptools.setup(name='easygan',
-      version='0.3',
+      version='0.4',
       author='hglad',
       author_email='hanseglad@gmail.com',
       package_dir={"easygan": "easygan"},
       packages=setuptools.find_packages(),
+      install_requires=['matplotlib>=3.3.4',
+                        'torchvision>=0.8.2',
+                        'torch>=1.7.1',
+                        'numpy>=1.19.2',
+                        'imageio>=2.9.0']
      )
